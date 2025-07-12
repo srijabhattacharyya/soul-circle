d654cd9dca10d6ca6e869cb072c3af00905b9ef3
Saibal Ganguly <tech@aimindia.org.in>
2025-07-12 14:44:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,9 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import dotenv from "dotenv";
+
+dotenv.config({ path: '.env.local' });
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
