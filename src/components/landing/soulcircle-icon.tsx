2d0178ff077fe8b2dc4d59dbf8f5125f74a0c02c
Saibal Ganguly <tech@aimindia.org.in>
2025-07-12 13:29:09 +0000

    some other icon for mental health counselling

--- src/components/landing/soulcircle-icon.tsx
+++ src/components/landing/soulcircle-icon.tsx
@@ -14,10 +14,10 @@ export function SoulCircleIcon(props: SVGProps<SVGSVGElement>) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M12 2a10 10 0 0 0-9.4 14.8" />
-      <path d="M12 22a10 10 0 0 0 9.4-14.8" />
-      <path d="M7 9h10" />
-      <path d="M7 15h10" />
+      <path d="M12 22a10 10 0 1 0-10-10" />
+      <path d="M12 2a10 10 0 1 0 10 10" />
+      <path d="M7 12c0 2.76 2.24 5 5 5s5-2.24 5-5" />
+      <path d="M10 9a2 2 0 1 1 4 0" />
     </svg>
   );
 }
