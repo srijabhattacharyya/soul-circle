fb5bbabebd8c765e54bf339717264620639943b6
Saibal Ganguly <tech@aimindia.org.in>
2025-07-13 04:18:51 +0000

    change policy in horizontal bar intolearn more

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -36,7 +36,7 @@ export function AppHeader() {
           )}
         >
           <BookOpen className="mr-2 h-4 w-4" />
-          Policies
+          Learn More
         </Link>
         <Link
           href="/about"
