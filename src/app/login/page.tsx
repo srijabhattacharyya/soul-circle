5e88c70ac0a8212ff60e5f6a3e5217139fd82f08
Saibal Ganguly <tech@aimindia.org.in>
2025-07-13 14:26:07 +0000

    Don't have an account? Sign Up: Please use font size same as sign in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -124,7 +124,7 @@ export default function LoginPage() {
         </form>
 
         <div className="mt-6">
-          <Button variant="link" onClick={() => setIsLoginView(!isLoginView)} className="text-base">
+          <Button variant="link" onClick={() => setIsLoginView(!isLoginView)} className="text-lg">
             {isLoginView ? "Don't have an account? Sign Up" : 'Already have an account? Sign In'}
           </Button>
         </div>
