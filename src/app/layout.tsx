b579d9f4f73dbb66431b695e409c98ea92234e95
Saibal Ganguly <tech@aimindia.org.in>
2025-07-13 01:17:23 +0000

    Please do not change the color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { AppHeader } from '@/components/app-header';
-import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { PT_Sans } from 'next/font/google';
 
